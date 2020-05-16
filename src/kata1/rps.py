0ebda44604cd2762500ff1c8cadc540f35b9a4b3
Fernan2 <fernando@rankia.com>
2020-05-16 18:02:04 +0200

    Implement quienGana to pass the tests

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,7 +7,19 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    player_choice = player.lower()
+    ai_choice = ai.lower()
+
+    if player_choice == ai_choice:
+        return "Empate!"
+    elif player_choice == "piedra" and ai_choice == "papel":
+        return "Perdiste!"
+    elif player_choice == "papel" and ai_choice == "tijeras":
+        return "Perdiste!"
+    elif player_choice == "tijeras" and ai_choice == "piedra":
+        return "Perdiste!"
+    else:
+        return "Ganaste!"
 
 # Entry Point
 def Game():
