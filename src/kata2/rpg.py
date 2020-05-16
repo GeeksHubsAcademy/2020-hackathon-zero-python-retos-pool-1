33b9582afa4e4e4519fa2036863c8f6c1a02baf6
OscarBurgos <burgoses@Oscar-PC.localdomain>
2020-05-16 21:18:11 +0200

    Primera prueba kata2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -5,9 +5,22 @@ import string
 
 def RandomPasswordGenerator(passLen=10):
     #
-    #
-    
-    #
-    #
+    #    
+    diccionario = string.ascii_letters + string.digits + string.punctuation
 
+    token_array = random.choices(diccionario,k=passLen)
+    
+    token = ''
+    for caracter in token_array:
+        token = token + caracter
+
+    #token = 'a'
+    #objeto = (chr in token for chr in string.ascii_letters)
+
+    #for kk in objeto:
+    #    print (kk)
+
+    return (token)
+    #
+    #
     return ""
