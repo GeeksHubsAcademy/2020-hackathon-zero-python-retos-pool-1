e027de3f141ddc317bdef35c88c7035ddcea99fc
Fernan2 <fernando@rankia.com>
2020-05-16 19:23:37 +0200

    Implement a Telegram bot for kata 3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,18 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    return "Hola, Geeks!"
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    return "Ayudame!"
 
 def mayus(update, context):
-        #
-        return ""
+        return context.args[0].upper()
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    #
-    return ""
+    return update.message.text[::-1]
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,19 +32,19 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1184879070:AAEl_0yqDNPkjoi0myazxa1-U2U_5g7ZkrE", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(CommandHandler("mayus", start))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
     #
-    
+
     # Y este espera al error
     dp.add_error_handler(error)
 
