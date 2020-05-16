d608bdd453df53be1ef1c296248815aaf94d2a14
jmbernalgomez <58164659+jmbernalgomez@users.noreply.github.com>
2020-05-16 19:53:11 +0200

    RETO 3 - Tu primer Bot
    José María Bernal

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,24 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    update.message.reply_text("Hola, Geeks!")
+    return "Hola, Geeks!"
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    update.message.reply_text("Ayudame!")
+    return "Ayudame!"
 
 def mayus(update, context):
-        #
-        return ""
+    texto = update.message.text.upper()
+    update.message.reply_text(texto)
+    return str(texto)
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    #
-    return ""
+    texto = update.message.text[::-1]
+    update.message.reply_text(texto)
+    return str(texto)
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,18 +38,18 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1271734615:AAEnAUIeTI3MDDUbv8mYeMKZke74-g5vGl8", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
-
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mayus", mayus))
+    
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    dp.add_handler(MessageHandler(Filters.text, alreves))
     
     # Y este espera al error
     dp.add_error_handler(error)
@@ -54,7 +58,6 @@ def main():
     updater.start_polling()
 
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
