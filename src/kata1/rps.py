0ffcec9b820f641004eaa37b3c58825343273ddd
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2020-05-16 10:00:46 -0500

    🚀 Kata1

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -2,22 +2,37 @@ from random import randint
 
 options = ["Piedra", "Papel", "Tijeras"]
 
+winers_rules = {"Piedra": "Tijeras", "Papel": "Piedra", "Tijeras": "Papel"}
 # El resultado de salida son las siguientes String
-#'Empate!'
-#'Ganaste!'
-#'Perdiste!'
+# 'Empate!'
+# 'Ganaste!'
+# 'Perdiste!'
+
+
 def quienGana(player, ai):
-    return ""
+    if(player == ai):
+        return "Empate!"
+    elif(ai == winers_rules[player]):
+        return "Ganaste!"
+    else:
+        return "Perdiste!"
 
 # Entry Point
+
+
 def Game():
     #
     #
-    
+    option_user = input("selecciones su opción : ")
+    option_ia = options[randint(0, 2)]
     #
     #
-    
-    winner = quienGana(player, ai)
+
+    winner = quienGana(option_user, option_ia)
+    print("Elegiste:", option_user)
+    print("AI:", option_ia)
 
     print(winner)
 
+
+Game()
