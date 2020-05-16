b6903e27b6556f44686acab83a85c6499bde88ff
Agnes20 <agnes19gomez@gmail.com>
2020-05-16 23:50:30 +0200

    master

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -13,12 +13,12 @@ def quienGana(player, ai):
     tijeras = '2'
     player = str(player)
     ai = str(ai)
-    if player == piedra:
+    if player == option[0]:
         if ai == piedra:
             resultado = 'Empate!'
         elif ai == tijeras:
             resultado = 'Ganaste!'
-    elif player == papel:
+    elif player == option[1]:
         if ai == papel:
             resultado = 'Empate!'
         elif ai == piedra:
