196ecbf44e02ac380cd6240beee3e672931d2985
jmbernalgomez <58164659+jmbernalgomez@users.noreply.github.com>
2020-05-16 18:52:33 +0200

    RETO 2 - Random Password Generator
    José María Bernal

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,9 +4,9 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
+    caracteres = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    longitud = passLen
+    cadena = ""   
     #
     #
 
