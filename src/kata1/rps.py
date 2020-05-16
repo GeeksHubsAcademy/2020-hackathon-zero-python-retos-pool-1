23061ba79b05845868bd69f9e75d4c896b93773b
RamonNav <65442305+RamonNav@users.noreply.github.com>
2020-05-16 19:26:25 +0200

    Add files via upload

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,16 +7,33 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    if player == ai:
+        return 'Empate!'
+    if (player == "Piedra" and ai == "Tijeras") or (player == "Papel" and ai == "Piedra") or (player == "Tijeras" and ai =="Papel"):
+        return 'Ganaste!'
+    else:
+        return 'Perdiste!'
 
 # Entry Point
 def Game():
-    #
-    #
-    
-    #
-    #
-    
+    print("elige entre piedra, papel o tijeras!")
+    selection = input()
+    if selection.lower() == "piedra":
+        player = "Piedra"
+    elif selection.lower() =="papel":
+        player = "Papel"
+    elif selection.lower() =="tijeras":
+        player = "Tijeras"
+    else:
+        print("seleccion incorrecta")
+
+    ai_number = random.randint(0,2)
+    if ai_number ==0:
+        ai = "Piedra"
+    elif ai_number ==1:
+        ai = "Papel"
+    else:
+        ai = "Tijeras"
     winner = quienGana(player, ai)
 
     print(winner)
--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,12 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
 
-    return ""
+    password = ""
+
+    password = password + "".join(random.choices(string.ascii_letters))
+    password = password + "".join(random.choices(string.digits))
+    password = password + "".join(random.choices(string.punctuation))
+    password = password + "".join(random.choices(string.ascii_letters + string.digits +string.punctuation, k=passLen-3))
+
+    return password
